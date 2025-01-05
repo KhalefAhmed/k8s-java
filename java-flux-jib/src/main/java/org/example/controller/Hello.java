db23a8123d881abae780db3e9c8f3d5349fdf3a4
KhalefAhmed <khalef.ahmedkhalil@gmail.com>
2025-01-06 00:22:23 +0100

    Add jib example :

---
+++ java-flux-jib/src/main/java/org/example/controller/Hello.java
@@ -0,0 +1,31 @@
+package org.example.controller;
+
+
+import org.example.model.Event;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.stream.Stream;
+
+@RestController
+public class Hello {
+
+    @GetMapping(value = "/", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Event> streamMessages() {
+
+        Flux<Event> eventFlux = Flux.fromStream(Stream.generate(() -> new Event("Hello from PMU!", LocalTime.now())));
+        Flux<Long> duratioFlux = Flux.interval(Duration.ofSeconds(1));
+        return Flux.zip(eventFlux, duratioFlux).map(Tuple2::getT1);
+
+    }
+
+    @GetMapping("/health")
+    public Event healthCheck() {
+        return new Event("Healthy", LocalTime.now());
+    }
+}
